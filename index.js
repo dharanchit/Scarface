e53fd1e2789c4bc72add9c7f0cbe140f3bd93179
dharanchit <anchit@1stmain.co>
2021-09-02 11:38:31 +0530

    PORT ADDED

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const common = require("./src/api/common/index");
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+const PORT = process.env.PORT || 4000;
+
 const connectToDb = async() => {
     try{
         await mongoose.connect(`mongodb+srv://anchit:${process.env.MONGO_PASSWORD}@cluster0.lpn0o.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,{
@@ -26,4 +28,4 @@ app.use("/api",populate);
 //API to fetch requested data
 app.use("/api",common);
 
-app.listen(4000, () => console.log("App is running on port 4000"));
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
